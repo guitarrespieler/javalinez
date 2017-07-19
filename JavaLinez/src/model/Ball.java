4402d2a14e6214e6ab5f8ecf01278b611b875efd
guitarrespieler <tibor.zsiga@hotmail.com>
2017-07-20 00:00:23 +0200

    haladások

--- JavaLinez/src/model/Ball.java
+++ JavaLinez/src/model/Ball.java
@@ -1,5 +1,10 @@
 package model;
 
+import model.enums.Color;
+
 public class Ball {
 
+	private Color color;
+	
+	private Position pos;
 }
---
+++ JavaLinez/src/model/Position.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Position {
+
+	private int x;
+	private int y;
+	
+	public Position(){}
+	
+	public Position(int xPos, int yPos){
+		x = xPos;
+		y = yPos;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		try{
+			Position other = (Position) obj;
+			
+			if((this.x == other.x) && (this.y == other.y))
+				return true;
+		}catch(ClassCastException e){}		
+		return false;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+}
