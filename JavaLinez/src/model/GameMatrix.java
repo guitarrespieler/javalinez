75c1c3d3b635b91d5d66400a800a042ed51257f3
guitarrespieler <tibor.zsiga@hotmail.com>
2017-07-20 00:50:13 +0200

    haladások

--- JavaLinez/src/model/GameMatrix.java
+++ JavaLinez/src/model/GameMatrix.java
@@ -6,5 +6,35 @@ public class GameMatrix {
 
 	private Ball[][] matrix = new Ball[rowCount][colCount];
 	
+	public boolean isFreePlace(Position pos){
+		if(isIndexingGood(pos)){
+			int row = pos.getY();
+			int col = pos.getX();
+			
+			if(matrix[row][col] == null)
+				return true;
+		}
+		return false;
+	}
 	
+	/**
+	 * @return true if the indexing is good
+	 * false anyways
+	 */
+	private boolean isIndexingGood(Position pos){
+		int x = pos.getX();
+		int y = pos.getY();
+		
+		if(x < 0 || x > colCount)
+			return false;
+		
+		if(y < 0 || y > rowCount)
+			return false;
+		
+		return true;
+	}
+
+	public Ball[][] getMatrix() {
+		return matrix;
+	}	
 }
