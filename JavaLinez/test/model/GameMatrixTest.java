d4e19a4379159a91ca3eb4166d32346b47217663
Zsiga Tibor <tibor.zsiga@hotmail.com>
2017-07-21 15:47:16 +0200

    test package

---
+++ JavaLinez/test/model/GameMatrixTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameMatrixTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
