625e6cbf424899c70995005b98c77c333487bf23
Zsiga Tibor <tibor.zsiga@hotmail.com>
2017-07-24 13:17:21 +0200

    tesztelgetés

--- JavaLinez/test/model/PathFinderTest.java
+++ JavaLinez/test/model/PathFinderTest.java
@@ -23,6 +23,29 @@ public class PathFinderTest {
 		manager.removeEveryBalls();
 		matrix = manager.getMap();
 	}
+	
+	@Test
+	public void pathExistsTest(){
+		Ball ball1 = new Ball(Color.getRandomColor(), new Position(0,0));		
+		matrix.addBall(ball1);
+		
+		Position destPos1 = new Position(0,5);		
+		PathDTO dto1 = PathFinder.findPath(ball1.getPos(), destPos1);		
+		assertTrue(dto1.isPathExist());
+		
+		Position destPos2 = new Position(5,5);		
+		PathDTO dto2 = PathFinder.findPath(ball1.getPos(), destPos2);		
+		assertTrue(dto2.isPathExist());
+		
+		Position destPos3 = new Position(GameMatrix.rowCount - 1 ,GameMatrix.colCount - 1);		
+		PathDTO dto3 = PathFinder.findPath(ball1.getPos(), destPos3);		
+		assertTrue(dto3.isPathExist());
+	}
+	
+	private void pathExists(Position actual, Position destination){
+		PathDTO dto = PathFinder.findPath(actual, destination);		
+		assertTrue(dto.isPathExist());
+	}
 
 	@Test
 	public void findPathTest1() {
@@ -42,9 +65,7 @@ public class PathFinderTest {
 		Position step4 = new Position(0,4);
 		Position step5 = new Position(0,5);
 		
-		checkPath(dto, step1, step2, step3, step4, step5);
-		
-		
+		checkPath(dto, step1, step2, step3, step4, step5);	
 	}
 	
 	private void checkPath(PathDTO dto, Position... expectedPositions){
