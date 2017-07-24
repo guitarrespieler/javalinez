64c99448f9ab1af673378aa3b8aeee77d35daaa0
Zsiga Tibor <tibor.zsiga@hotmail.com>
2017-07-24 13:07:22 +0200

    tesztelgetés

--- JavaLinez/test/model/PathFinderTest.java
+++ JavaLinez/test/model/PathFinderTest.java
@@ -2,6 +2,9 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,10 +30,29 @@ public class PathFinderTest {
 		
 		matrix.addBall(ball1);
 		
-		Position destPos = new Position(5,5);
+		Position destPos = new Position(0,5);
 		
 		PathDTO dto = PathFinder.findPath(ball1.getPos(), destPos);
 		
+		assertTrue(dto.isPathExist());
+		
+		Position step1 = new Position(0,1);
+		Position step2 = new Position(0,2);
+		Position step3 = new Position(0,3);
+		Position step4 = new Position(0,4);
+		Position step5 = new Position(0,5);
+		
+		checkPath(dto, step1, step2, step3, step4, step5);
+		
+		
 	}
+	
+	private void checkPath(PathDTO dto, Position... expectedPositions){
+		List<Position> actualPath = dto.getPath();
+		for(int i = 0; i < expectedPositions.length && i < actualPath.size(); i++){
+			assertEquals(expectedPositions[i], actualPath.get(i));
+		}
+	}
+	
 
 }
